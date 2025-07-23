6c573943b759ed5d5557566c1a24afecb436536d
goring <dlrkdms001@gmail.com>
2025-07-23 22:38:32 +0900

    upload 3307

--- goring/src/week01/SWEA_3307.java
+++ goring/src/week01/SWEA_3307.java
@@ -17,7 +17,7 @@ public class SWEA_3307 {
 			for(int i=0; i<N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			
+		
 			for(int i=0; i<N-1; i++) {
 				for(int j=i+1; j<N; j++) {
 					if(arr[i] < arr[j]) {
