fdd1b5cb1f093eea68473f802a23fb30088f9a93
Teddysir <sdzx0719@naver.com>
2025-07-24 21:01:01 +0900

    feat: SWEA_9229 Solution

--- Teddysir/src/week01/day_0723/최장증가부분수열_3307.java
+++ Teddysir/src/week01/day_0723/최장증가부분수열_3307.java
@@ -6,37 +6,38 @@ import java.util.*;;
 public class 최장증가부분수열_3307 {
 
 	static int T, N, max;
-	static int[] arr;
-	static int[] ans;
+	static int[] arr, dp;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String args[]) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		T = Integer.parseInt(br.readLine());
-
 		for (int i = 1; i <= T; i++) {
+
 			N = Integer.parseInt(br.readLine());
+			max = 0;
+			arr = new int[N];
+			dp = new int[N];
+			for (int j = 0; j < N; j++) {
+				dp[j] = 1;
+			}
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr = new int[N];
-			ans = new int[N];
-
 			for (int j = 0; j < N; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
-				ans[j] = 1; // 1로 초기화해줘야함 
 			}
 			for (int j = 1; j < N; j++) {
 				for (int k = 0; k < j; k++) {
-					if (arr[k] < arr[j]) {
-						ans[j] = Math.max(ans[j], ans[k] + 1);
+					if (arr[j] > arr[k]) {
+						dp[j] = Math.max(dp[j], dp[k] + 1);
 					}
 				}
 			}
 
-			for (int j = 0; j < ans.length; j++) {
-				max = Math.max(max, ans[j]);
+			for (int j = 0; j < dp.length; j++) {
+				max = Math.max(max, dp[j]);
 			}
 
 			sb.append("#").append(i).append(" ").append(max).append("\n");
